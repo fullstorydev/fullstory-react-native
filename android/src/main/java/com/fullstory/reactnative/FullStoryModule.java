54f9a901c2c3a3eb0cacbfb525bda193a2a17c16
Josh Miers <joshmiers@fullstory.com>
2020-07-18 09:01:54 -0400

    formatting

--- android/src/main/java/com/fullstory/reactnative/FullStoryModule.java
+++ android/src/main/java/com/fullstory/reactnative/FullStoryModule.java
@@ -1,21 +1,18 @@
-
 package com.fullstory.reactnative;
-
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
-
 import com.fullstory.FS;
 import com.fullstory.FSOnReadyListener;
 import com.fullstory.FSSessionData;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class FullStoryModule extends ReactContextBaseJavaModule {
 
@@ -124,6 +121,5 @@ public class FullStoryModule extends ReactContextBaseJavaModule {
         }
 
         return map.toHashMap();
-
     }
 }
--- android/src/main/java/com/fullstory/reactnative/NativeProp.java
+++ android/src/main/java/com/fullstory/reactnative/NativeProp.java
@@ -1,16 +1,16 @@
 package com.fullstory.reactnative;
 
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 @Retention(RUNTIME)
 @Target(ElementType.METHOD)
 public @interface NativeProp {
- /**
-   * Name of the property exposed to JS
-   */
-  String name();
+    /**
+     * Name of the property exposed to JS
+     */
+    String name();
 }
