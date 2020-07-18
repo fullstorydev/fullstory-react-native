f22d4fae479b6d45355561798c80f8580feee29e
Josh Miers <joshmiers@fullstory.com>
2020-07-18 09:02:19 -0400

    clear existing values when fsAttribute and fsClass are set

--- android/src/main/java/com/fullstory/reactnative/FullStoryNativeProps.java
+++ android/src/main/java/com/fullstory/reactnative/FullStoryNativeProps.java
@@ -1,26 +1,43 @@
 package com.fullstory.reactnative;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
-
 import android.view.View;
 
 import com.facebook.react.bridge.ReadableMap;
-
 import com.fullstory.FS;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class FullStoryNativeProps {
+    // Keeps a list of the attributes set on a given View, so we can clear out the attributes
+    // any time set_fsAttribute is called.
+    private static final WeakHashMap<View, List<String>> ATTR_LIST = new WeakHashMap<>();
+
     private FullStoryNativeProps() {
         // prevent instantiation
     }
 
-    @NativeProp(name="fsAttribute")
+    @NativeProp(name = "fsAttribute")
     public static void set_fsAttribute(View view, ReadableMap fsAttributes) {
+        // Always clear out the existing attributes first.
+        // Note that, since the dataComponent, dataElement, and dataSourceFile values are stores as
+        // attributes, we can't just clear out all existing attributes. Instead, we must keep track
+        // of all of the attributes that were set and remove them individually.
+        List<String> existingAttributes = ATTR_LIST.remove(view);
+        if (existingAttributes != null) {
+            for (String attr : existingAttributes) {
+                FS.removeAttribute(view, attr);
+            }
+        }
         if (fsAttributes == null) {
             return;
         }
 
+        List<String> newAttributes = new ArrayList<>();
         Iterator<Map.Entry<String, Object>> it = fsAttributes.getEntryIterator();
         while (it.hasNext()) {
             Map.Entry<String, Object> entry = it.next();
@@ -34,12 +51,20 @@ public class FullStoryNativeProps {
                 value = obj.toString();
             }
 
-            FS.setAttribute(view, entry.getKey(), value);
+            String key = entry.getKey();
+            newAttributes.add(key);
+            FS.setAttribute(view, key, value);
         }
+
+        // Store the new list of attributes.
+        ATTR_LIST.put(view, newAttributes);
     }
 
-    @NativeProp(name="fsClass")
+    @NativeProp(name = "fsClass")
     public static void set_fsClass(View view, String fsClass) {
+        // always clear out the existing classes first
+        FS.removeAllClasses(view);
+
         if (fsClass == null) {
             // nothing to do
             return;
@@ -59,22 +84,22 @@ public class FullStoryNativeProps {
         }
     }
 
-    @NativeProp(name="fsTagName")
+    @NativeProp(name = "fsTagName")
     public static void set_fsTagName(View view, String fsTagName) {
         FS.setTagName(view, fsTagName);
     }
 
-    @NativeProp(name="dataComponent")
+    @NativeProp(name = "dataComponent")
     public static void set_dataComponent(View view, String value) {
         setElementIdentity(view, "data-component", value);
     }
 
-    @NativeProp(name="dataElement")
+    @NativeProp(name = "dataElement")
     public static void set_dataElement(View view, String value) {
         setElementIdentity(view, "data-element", value);
     }
 
-    @NativeProp(name="dataSourceFile")
+    @NativeProp(name = "dataSourceFile")
     public static void set_dataSourceFile(View view, String value) {
         setElementIdentity(view, "data-source-file", value);
     }
