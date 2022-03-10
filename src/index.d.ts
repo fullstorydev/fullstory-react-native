c5f691e93784eb1b3a592d126c69feb8943ebdd7
Josh Miers <joshmiers@fullstory.com>
2022-03-10 15:27:06 -0500

    trying to define OnReady properly

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,11 +2,17 @@ import {NativeModules} from 'react-native';
 
 declare const {FullStory} = NativeModules;
 
+type OnReadyResponse = {
+  replayStartUrl: string;
+  replayNowUrl: string;
+  sessionId: string;
+};
+
 interface FullStoryInterface {
   anonymize(): void;
   identify(string, Object): void;
   setUserVars(Object): void;
-  onReady(): Promise;
+  onReady(): Promise<OnReadyResponse>;
   getCurrentSession(): Promise<string>;
   getCurrentSessionURL(): Promise<string>;
   consent(boolean): void;
