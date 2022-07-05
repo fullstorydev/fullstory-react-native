9e7b4ea6c6c747639042e67a9beea27f14a416f4
Ryan Wang <ryanwang@fullstory.com>
2022-07-05 13:47:37 -0400

    remove blank value settings

--- plugin/src/withFullStoryAndroid.ts
+++ plugin/src/withFullStoryAndroid.ts
@@ -73,9 +73,9 @@ export const addFullStoryGradlePlugin = (
     newSrc: `apply plugin: 'fullstory'
       fullstory {
           org '${org}'
-          server ${host ? `'https://${host}'` : ""}
-          logLevel ${logLevel ? `'${logLevel}'` : ""}
-          enabledVariants ${enabledVariants ? `'${enabledVariants}'` : ""}
+          ${host ? `server 'https://${host}'` : ""}
+          ${logLevel ? `logLevel '${logLevel}'` : ""}
+          ${enabledVariants ? `enabledVariants '${enabledVariants}'` : ""}
       }`,
     anchor: /./,
     offset: 1,
