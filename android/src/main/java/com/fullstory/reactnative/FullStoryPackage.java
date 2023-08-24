0f440ec4f46cd46b856c5556642a70a888c382ce
Ryan Wang <qwang1993@gmail.com>
2023-08-24 13:15:33 -0400

    fix turbo module compatibility (#73)
    Co-authored-by: Ryan Wang <ryanwang@fullstory.com>

--- android/src/main/java/com/fullstory/reactnative/FullStoryPackage.java
+++ android/src/main/java/com/fullstory/reactnative/FullStoryPackage.java
@@ -2,6 +2,7 @@ package com.fullstory.reactnative;
 
 import androidx.annotation.Nullable;
 
+import com.facebook.react.ReactPackage;
 import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -11,7 +12,7 @@ import com.facebook.react.module.model.ReactModuleInfoProvider;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FullStoryPackage extends TurboReactPackage {
+public class FullStoryPackage extends TurboReactPackage implements ReactPackage {
     @Nullable
     @Override
     public NativeModule getModule(String name, ReactApplicationContext reactContext) {
@@ -24,21 +25,25 @@ public class FullStoryPackage extends TurboReactPackage {
 
     @Override
     public ReactModuleInfoProvider getReactModuleInfoProvider() {
-        return () -> {
-            final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
-            boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-            moduleInfos.put(
-                    FullStoryModuleImpl.NAME,
-                    new ReactModuleInfo(
-                            FullStoryModuleImpl.NAME,
-                            FullStoryModuleImpl.NAME,
-                            false, // canOverrideExistingModule
-                            false, // needsEagerInit
-                            true, // hasConstants
-                            false, // isCxxModule
-                            isTurboModule // isTurboModule
-            ));
-            return moduleInfos;
+        final Map<String, ReactModuleInfo> moduleInfos = new HashMap<>();
+        boolean isTurboModule = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        moduleInfos.put(
+                FullStoryModuleImpl.NAME,
+                new ReactModuleInfo(
+                        FullStoryModuleImpl.NAME,
+                        FullStoryModuleImpl.NAME,
+                        false, // canOverrideExistingModule
+                        false, // needsEagerInit
+                        true, // hasConstants
+                        false, // isCxxModule
+                        isTurboModule // isTurboModule
+        ));
+
+        return new ReactModuleInfoProvider() {
+            @Override
+            public Map<String, ReactModuleInfo> getReactModuleInfos() {
+                return moduleInfos;
+            }
         };
     }
 }
