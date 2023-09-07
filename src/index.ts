f2ad34517c575be1091f720dac3aa906be00c0f5
Ryan Wang <ryanwang@fullstory.com>
2023-09-07 12:12:08 -0400

    Provide identify with default userVars

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ export type OnReadyResponse = {
 declare type FullStoryStatic = {
   LogLevel: typeof LogLevel;
   anonymize(): void;
-  identify(uid: string, userVars: UserVars): void;
+  identify(uid: string, userVars?: UserVars): void;
   setUserVars(userVars: UserVars): void;
   onReady(): Promise<OnReadyResponse>;
   getCurrentSession(): Promise<string>;
@@ -69,9 +69,11 @@ declare global {
   }
 }
 
+const identifyWithProperties = (uid: string, userVars = {}) => identify(uid, userVars);
+
 const FullStoryAPI: FullStoryStatic = {
   anonymize,
-  identify,
+  identify: identifyWithProperties,
   setUserVars,
   onReady,
   getCurrentSession,
