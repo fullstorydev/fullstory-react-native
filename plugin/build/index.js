b431ed0b25dae6217b916329ae3c2bf35783fa70
Ryan Wang <ryanwang@fullstory.com>
2024-05-13 15:37:39 -0400

    update test and build files

--- plugin/build/index.js
+++ plugin/build/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config_plugins_1 = require("@expo/config-plugins");
 const withFullStoryAndroid_1 = __importDefault(require("./withFullStoryAndroid"));
 const withFullStoryIos_1 = __importDefault(require("./withFullStoryIos"));
-const pkg = require("../../package.json");
+const pkg = require('../../package.json');
 const withFullStory = (config, pluginConfigs) => {
     if (!pluginConfigs.org) {
         throw new Error("Please specify an 'org' in your plugin arguments.");
--- plugin/build/withFullStoryAndroid.d.ts
+++ plugin/build/withFullStoryAndroid.d.ts
@@ -1,7 +1,7 @@
-import { ConfigPlugin } from "@expo/config-plugins";
-import { FullStoryAndroidProps } from ".";
+import { ConfigPlugin } from '@expo/config-plugins';
+import { FullStoryAndroidProps } from '.';
 export declare const addFullStoryMavenRepo: (projectBuildGradle: string) => string;
 export declare const addFullStoryProjectDependency: (projectBuildGradle: string, version: string) => string;
-export declare const addFullStoryGradlePlugin: (appBuildGradle: string, { org, host, logLevel, logcatLevel, enabledVariants, recordOnStart, }: FullStoryAndroidProps) => string;
+export declare const addFullStoryGradlePlugin: (appBuildGradle: string, { org, host, logLevel, logcatLevel, enabledVariants, recordOnStart, version, }: FullStoryAndroidProps) => string;
 declare const withFullStoryAndroid: ConfigPlugin<FullStoryAndroidProps>;
 export default withFullStoryAndroid;
