70e87cd6bbced45c3c78806ea1843ad0f5fbabb9
Ryan Wang <qwang1993@gmail.com>
2025-10-02 10:14:12 -0400

    swizzle LottieAnimationViewComponentView

--- ios/FullStory.mm
+++ ios/FullStory.mm
@@ -394,6 +394,7 @@ static bool array_contains_string(const char **array, const char *string) {
     SWIZZLE_HANDLE_COMMAND(RCTPullToRefreshViewComponentView);
     SWIZZLE_HANDLE_COMMAND(RCTLegacyViewManagerInteropComponentView);
     SWIZZLE_HANDLE_COMMAND(RNCPagerViewComponentView);
+    SWIZZLE_HANDLE_COMMAND(LottieAnimationViewComponentView);
 #pragma clang pop
 
 #pragma clang diagnostic push
