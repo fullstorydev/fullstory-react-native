eb6a782da0fd25677d3475d121b7b915b292fd69
Ryan Wang <qwang1993@gmail.com>
2025-11-18 16:46:55 -0500

    set up jest to receive babel rewrites

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    '@fullstory/babel-plugin-react-native',
+    ['@fullstory/babel-plugin-annotate-react', { native: true }],
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  setupFiles: ['<rootDir>/setupTests.js'],
+  setupFiles: ['<rootDir>/setupTests.ts'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  transform: {
-    '^.+\\.(js)$': 'babel-jest',
-    '\\.(ts|tsx)$': 'ts-jest',
-  },
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(@react-native|react-native)/).*/'],
+  transform: { '^.+\\.(js)$': 'babel-jest' },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(@react-native|react-native|react)/).*/'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/plugin', '<rootDir>/lib'],
+  moduleNameMapper: {
+    '^@fullstory/react-native$': '<rootDir>/src/index.ts',
+  },
 };
